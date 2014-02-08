3c354dfc025e02d9112fe8ca84826338ab753525
Chris Van Horne <chris.vanhorne@gmail.com>
2014-02-08 12:53:58 -0700

    SOmething

--- poem.js
+++ poem.js
@@ -2,7 +2,6 @@ function nextLine(line) {
 
 	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on github</p>",
 "<p onClick=nextLine(2)>So many pull requests she did snub</p>",
-"<p onClick=nextLine(5)>Said the maintainer, 'Welcome to the club!'</p>");
 
 	document.getElementById("line").innerHTML=poem[line];
 }
